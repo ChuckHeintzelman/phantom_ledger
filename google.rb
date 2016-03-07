ffd6b0326c91a4975623bd4e419ca8d0e1bbe7c5
Christopher Tarwater <ctarwater@gmail.com>
2016-03-07 14:44:07 -0800

    change google script to use FF instead of phantomjs for testing

--- google.rb
+++ google.rb
@@ -6,10 +6,10 @@ require "fileutils"
 capabilities = Selenium::WebDriver::Remote::Capabilities.phantomjs("phantomjs.page.settings.userAgent" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36")
 
 driver = Selenium::WebDriver.for :phantomjs, :desired_capabilities => capabilities
-browser = Watir::Browser.new driver
+#browser = Watir::Browser.new driver
 
 # Test with GUI Browser
-#browser = Watir::Browser.new(:firefox)
+browser = Watir::Browser.new(:firefox)
 
 # Go to the login page
 browser.goto 'https://play.google.com/books/publish/redirect'
